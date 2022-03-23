2590ee9b001f52171ffc35e58230425296b7e7b7
Irina <irina@netmagik.com>
2022-03-23 11:35:39 -0400

    Add Contact Section

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,17 +1,29 @@
 import React from "react";
 import './contact.css';
+import {AiOutlineMail} from "react-icons/ai";
 
 const Contact = () => {
     return (
         <section id="contact">
-        <div className="contact-container">
-            <h2>Contact</h2>
-            <div className="contact-info">
-            <div className="contact-info-container">
-                <h3>Email</h3>
-             </div>
-             </div>
-        </div>
+            <h5>Get In Touch</h5>
+            <h2>Contact Me</h2>
+            <div className="container contact-container">
+                <div className="contact-options">
+                    <article className="contact-option">
+                        <AiOutlineMail className="contact-icon"/>
+                        <h4>Email</h4>
+                        <a href="mailto:irina@netmagik.com" target="_blank">Send a message</a>
+                    </article>
+                    
+                </div>
+                {/* End of Contact Options */}
+                <form action="">
+                    <input type="text" name="name" placeholder="Your Full Name" required />
+                    <input type="email" name="email" placeholder="Your Email" required />
+                    <textarea name="message" placeholder="Your Message" cols="30" rows="7" required></textarea>
+                    <button type="submit" className="btn btn-primary">Send Message</button>
+                </form>
+            </div>
         </section>
 
     );
