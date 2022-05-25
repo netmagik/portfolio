86f452404d6b3920170ee33ffb9d0b2b84a13638
Irina <irina@netmagik.com>
2022-05-25 09:32:13 -0400

    Add Description to Projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -18,6 +18,7 @@ const Projects = () => {
             id: 1,
             image: CovidChart,
             title: 'Covid-19 Chart',
+            desc: 'Covid-19 Cases by Country, State, pulled from data provided by API. Chart of daily data in the US. Ability to sort data by number of cases, deaths, country, state.',
             github: 'https://github.com/netmagik/Covid-Tracker',
             demo: 'https://netmagik.github.io/Covid-Tracker',
         },
@@ -25,6 +26,7 @@ const Projects = () => {
             id: 2,
             image: Weather,
             title: 'Weather App',
+            desc: 'Get local weather by location. Toggle b/t Celcius and Fahrenheit. Get 5 day forecast. Search by City Name',
             github: 'https://github.com/netmagik/Weather-App',
             demo: 'https://netmagik.github.io/Weather-App/',
         },
@@ -32,6 +34,7 @@ const Projects = () => {
             id: 3,
             image: Wiki,
             title: 'Wikipedia Search',
+            desc: 'Search Wikipedia by using Wiki API, or use Random button to load a Random article.',
             github: 'https://github.com/netmagik/Wiki-Search',
             demo: 'https://netmagik.github.io/Wiki-Search/',
         },
@@ -39,6 +42,7 @@ const Projects = () => {
             id: 4,
             image: Shopping,
             title: 'Shopping Cart',
+            desc: 'CRUD App with React Hooks. Shopping Cart with predefined products has the ability to update product quantity, calculate price and delete all',
             github: 'https://github.com/netmagik/shopping-cart',
             demo: 'https://netmagik.github.io/shopping-cart/',
         },
@@ -46,6 +50,7 @@ const Projects = () => {
             id: 5,
             image: Subreddit,
             title: 'Subreddit Viewer',
+            desc: 'Search for Subreddits and load articles using Reddit API.',
             github: 'https://github.com/netmagik/Subreddit-feed-app',
             demo: 'https://netmagik.github.io/Subreddit-feed-app/',
         },
@@ -53,6 +58,7 @@ const Projects = () => {
             id: 6,
             image: DataGrid,
             title: 'React DataGrid',
+            desc: 'Displays data from JSON Placeholder and X-Grid-Data-Generator in React DataGrid and React Card Component. React Router. ',
             github: 'https://github.com/netmagik/datagrid',
             demo: 'https://netmagik.github.io/datagrid/',
         }
@@ -63,24 +69,28 @@ const Projects = () => {
             id: 1,
             image: AI,
             title: 'UCF AI Initiative',
+            desc: 'Website to highlight AI activities at UCF. It features faculty directory and their contribution to AI research at UCF. Listing of AI research projects, areas of interest, educational programs, AI courses taught and contact information. ',
             about: 'https://www.netmagik.com/portfolio/website-for-ucf-ai-initiative/',
         },
         {
             id: 2,
             image: Casl,
             title: 'UCF Research Lab',
+            desc: 'Website for CASL Research Lab, specializing in Data Analytics. It features research projects, papers, faculty directory & information about MS in Data Analytics.',
             about: 'https://www.netmagik.com/portfolio/website-for-ucf-research-laboratory/',
         },
         {
             id: 3,
             image: Eye,
             title: 'Eye Physicians',
+            desc: 'Website for a large medical practice providing information to patients, referring doctors and research studies.',
             about: 'https://www.netmagik.com/portfolio/website-medical-practice/',
         },
         {
             id: 4,
             image: Disney,
             title: 'Disney Travel',
+            desc: 'Website for a busy Disney travel agency providing information to clients interested in Disney Travel.',
             about: 'https://www.netmagik.com/portfolio/website-for-disney-travel-agency/',
         },
     ]
@@ -121,7 +131,7 @@ const Projects = () => {
                                 </div>
                                 <h3>{title}</h3>
                                 <div className="projects-item-cta">
-                                    <a href={about} target="_blank" className="btn about">About The Project
+                                    <a href={about} target="_blank" className="btn about">Learn More
                                     </a>                                
                                 </div>
                             </article>
