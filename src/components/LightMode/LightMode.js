ec4d87e2a957963b122264d3f919d8336b28e1b8
Irina <irina@netmagik.com>
2022-05-27 11:16:42 -0400

    Create Light Mode Component and CSS

---
+++ src/components/LightMode/LightMode.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '/.lightmode.css';
+
+
+const Toggle = () => {
+    return (
+        <div className="toggle-wrapper">
+            <span>☀️</span>
+            <label className="toggle-theme" htmlFor="checkbox">
+                <input
+                    type="checkbox"
+                    id="checkbox"
+                />
+                <div className="slider round"></div>
+            </label>
+            <span>🌒</span>
+        </div>
+    )
+}  
+
+export default Toggle;  
+    
