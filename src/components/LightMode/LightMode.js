95f9542069a5fe4160f630732cf7d9367057c6ba
Irina <irina@netmagik.com>
2022-05-27 12:21:52 -0400

    Add code for Toggle Button

--- src/components/LightMode/LightMode.js
+++ src/components/LightMode/LightMode.js
@@ -1,8 +1,39 @@
 import React from 'react';
 import './lightmode.css';
 
+    const setDark = () => {
+        localStorage.setItem('theme', 'dark');
+        document.documentElement.setAttribute('data-theme', 'dark');
+    };
+
+    const setLight = () => {
+        localStorage.setItem('theme', 'light');
+        document.documentElement.setAttribute('data-theme', 'light');
+    };
+
+    const currentTheme = localStorage.getItem('theme');
+
+    const prefersDark = 
+        window.matchMedia &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    const defaultDark = 
+        currentTheme === "dark" || (currentTheme === null && prefersDark);
+
+    if (defaultDark) {
+        setDark();
+    }
+
+    const toggleTheme = (e) => {
+        if (e.target.checked) {
+            setLight();
+        } else {
+            setDark();
+        }
+    };
 
 const Toggle = () => {
+
     return (
         <div className="toggle-wrapper">
             <span>🌒</span>
@@ -10,6 +41,8 @@ const Toggle = () => {
                 <input
                     type="checkbox"
                     id="checkbox"
+                    onChange={toggleTheme}
+                    defaultChecked={defaultDark}
                 />
                 <div className="slider round"></div>
             </label>
