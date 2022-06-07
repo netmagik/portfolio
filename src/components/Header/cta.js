aafdee8484adbb706cfbb62a7c38da8513c2b27c
Irina <irina@netmagik.com>
2022-06-07 13:45:16 -0400

    Add Resume

--- src/components/Header/cta.js
+++ src/components/Header/cta.js
@@ -3,7 +3,7 @@ import React from "react";
 const cta = () => {
     return (
         <div className="cta">
-            <a className="btn" href="#">Download Resume</a>
+            <a className="btn" href="https://www.irinablumenfeld.com/static/media/Resume.pdf">Download Resume</a>
             <a className="btn-primary" href="#contact">Contact</a>
         </div>
 
